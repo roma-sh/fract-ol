2e1ce23cf9f8f4e1450df1856741688fe295249c
Ramez Shatra <rshatra@2-F-3.42heilbronn.de>
2024-06-16 04:51:43 +0200

    both of fractals were drawn and shown into the window

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: rshatra <rshatra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 07:56:26 by rshatra           #+#    #+#             */
-/*   Updated: 2024/06/14 20:50:56 by rshatra          ###   ########.fr       */
+/*   Updated: 2024/06/16 04:46:27 by rshatra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,13 @@
 
 # define SIZE 800
 
+typedef struct s_pixel
+{
+	int32_t		w;
+	int32_t		h;
+	uint32_t	color;
+}	t_pixel;
+
 typedef struct s_complex
 {
 	double			re;
@@ -35,12 +42,24 @@ typedef struct s_fractol
 	mlx_image_t		*mlx_img;
 	mlx_t			*mlx;
 	t_complex		c;
+	t_complex		b;
+	unsigned int	(*f)(struct s_fractol *fractol, t_complex *c);
+	double			zoom;
+	double			axis_len;
+	uint32_t		max_iter;
+	int				rgb[3];
 }	t_fractol;
 
 int					ft_strncmp(const char *str1, const char *str2, size_t x);
-double				ft_atof(const char *str);
+double				ft_atof(const char *s);
 void				init_julia(t_fractol *fractol, char *s1, char *s2);
 void				init_mandelbrot(t_fractol *fractol);
 int					init(t_fractol *fractol);
+void				ztrans(t_complex *z, t_fractol *fractol, t_pixel *pixel);
+uint32_t			julia_set(t_fractol *fractol, t_complex *c);
+uint32_t			mandelbrot_set(t_fractol *fractol, t_complex *c);
+int32_t				bernstein_color(double x, int *rgb);
+int32_t				pixel_color(int32_t r, int32_t g, int32_t b, int32_t a);
+void				draw(void *param);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,24 +6,42 @@
 /*   By: rshatra <rshatra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 17:18:18 by rshatra           #+#    #+#             */
-/*   Updated: 2024/06/14 20:46:52 by rshatra          ###   ########.fr       */
+/*   Updated: 2024/06/16 04:06:47 by rshatra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+void	draw(void *param)
+{
+	t_fractol	*fractal;
+	t_pixel		pixel;
+	t_complex	z;
+
+	fractal = param;
+	pixel.h = 0;
+	while (pixel.h < SIZE)
+	{
+		pixel.w = 0;
+		while (pixel.w < SIZE)
+		{
+			ztrans(&z, fractal, &pixel);
+			pixel.color = fractal->f(fractal, &z);
+			mlx_put_pixel(fractal->mlx_img, pixel.w, pixel.h, pixel.color);
+			pixel.w++;
+		}
+		pixel.h++;
+	}
+}
 
 int	main(int argc, char **argv)
 {
 	t_fractol	fractol;
 
 	if (argc == 4 && !ft_strncmp(argv[1], "julia", 5))
-	{
 		init_julia(&fractol, argv[2], argv[3]);
-	}
-	else if (argc == 2 && !ft_strncmp(argv[1], "mandelbort", 10))
-	{
+	else if (argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10))
 		init_mandelbrot(&fractol);
-	}
 	else
 	{
 		write(1, "please choose the correct fractol:\n", 35);
@@ -36,6 +54,7 @@ int	main(int argc, char **argv)
 		write(1, "Error\n", 6);
 		exit(EXIT_FAILURE);
 	}
+	mlx_loop_hook(fractol.mlx, draw, &fractol);
 	mlx_loop(fractol.mlx);
 	mlx_terminate(fractol.mlx);
 	return (0);
